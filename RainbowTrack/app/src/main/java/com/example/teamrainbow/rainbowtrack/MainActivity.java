6f46c087e9f3984562596d45dfc2995d402ea776
Andrius Sakalas <gussakan@student.gu.se>
2017-11-18 14:24:21 +0100

    WS

--- RainbowTrack/app/src/main/java/com/example/teamrainbow/rainbowtrack/MainActivity.java
+++ RainbowTrack/app/src/main/java/com/example/teamrainbow/rainbowtrack/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.teamrainbow.rainbowtrack;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class MainActivity extends AppCompatActivity {
+    private WebSocketClient mWebSocketClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        connectWebSocket();
         Button goToUI = (Button) findViewById(R.id.button_goToUI);
         goToUI.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,4 +31,45 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    private void connectWebSocket() {
+        URI uri;
+        try {
+            uri = new URI("ws://websockethost:8080");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        mWebSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                mWebSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+            }
+
+            @Override
+            public void onMessage(String s) {
+                // final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //  TextView textView = (TextView)findViewById(R.id.messages);
+                        //   textView.setText(textView.getText() + "\n" + message);
+                    }
+                });
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+        mWebSocketClient.connect();
+    }
 }
+
